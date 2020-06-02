31ce6794991db3d50f0fde437da2c1b23a8b6c5d
soulless12368 <soullessGamer@yandex.ua>
2020-06-02 20:46:52 +0300

    hello

---
+++ app/src/main/java/com/example/kursach_4_0/adapter/MyThirdRecycleViewAdapter.java
@@ -0,0 +1,86 @@
+package com.example.kursach_4_0.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kursach_4_0.R;
+import com.example.kursach_4_0.SecondActivity;
+
+import java.util.List;
+
+public class MyThirdRecycleViewAdapter extends RecyclerView.Adapter<MyThirdRecycleViewAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public MyThirdRecycleViewAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycleview_column, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String town = mData.get(position);
+        holder.myTextView.setText(town);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.column3textView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+
+        }
+    }
+
+    // convenience method for getting data at click position
+    public String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(SecondActivity itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public void handleClick(Context context) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        context.startActivity(intent);
+    }
+}
